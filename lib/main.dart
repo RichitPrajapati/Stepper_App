2a29171b855b31d5054b63f58cbbab9869992cd3
RichitPrajapati <richit8441@gmail.com>
2022-10-31 11:30:14 +0530

    master

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:stepper/Home_Page.dart';
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home_Page(),
+    ),
+  );
+}
